b3bba93fa8c30461c665b25a5813f668ef6ae5f0
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-09-30 08:03:12 -0500

    cleaned up test method for moving files

--- watch_downloads.py
+++ watch_downloads.py
@@ -11,21 +11,29 @@ class Settings(BaseSettings):
     class Config:
             env_file = ".env"
 
-settings = Settings()
+ENV = Settings()
 
 # if watch url == log url get a new one
 
 def get_log():
-     with open(f'{settings.log_url}/changes.txt', 'r') as log:
+     with open(f'{ENV.log_url}/changes.txt', 'r') as log:
            data = log.read()
            return data
 
 def write_log(entry):
-        with open(f'{settings.log_url}/changes.txt', 'w') as log:
+        with open(f'{ENV.log_url}/changes.txt', 'w') as log:
             log.write(f"{data}\n{entry}")
 
 
-for changes in watch(settings.watch_dir):
+def make_folder(root, tag):
+    folder = f"{root}/{tag}/"
+    if os.path.exists(folder):
+        pass
+    else:
+        os.mkdir(folder)
+    return folder
+
+for changes in watch(ENV.watch_dir):
     
     now = datetime.now()
     data = get_log()
@@ -37,15 +45,12 @@ for changes in watch(settings.watch_dir):
         action = str(entry[0]).split('.')[1]
         file_path = entry[1]
         file_name = os.path.basename(file_path)
-        log_entry = f"{str(now)[0:str(now).find('.', 0)]} -- {file_path} ---> {action}"
-        
-        print(file_name)
-        # first log files
-        write_log(log_entry)
 
+        write_log(f"{str(now)[0:str(now).find('.', 0)]} -- {file_path} ---> {action}")
 
         if file_path.endswith("txt") and action == "added":
-            os.rename(file_path, f'C:/Users/willm/Desktop/watchgod/test/{file_name}')
+            new_file_path = make_folder(ENV.dest_dir_root, "Documents")
+            os.rename(file_path, new_file_path + file_name)
 
 # os.rename("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
 #  f"{settings.dest_dir_root}/"
